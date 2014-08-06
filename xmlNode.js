5c1f579ac8c59c553d9f5f9e89abb20304557b68
Alexander Brett <alexander.brett@sophos.com>
2014-08-06 12:03:32 +0100

    Document & Bugfix
    Add docstrings, replace instantiation check using instanceof

--- xmlNode.js
+++ xmlNode.js
@@ -1,6 +1,13 @@
 xmlNode = function (xmlString, identifiers, depth, parentID) {
-  if(this === window){ return new xmlNode(xmlString, identifiers, depth, parentID)}
-  function repeat(x,n){var s='';for (;;) {if(n&1)s+=x;n>>=1;if(n) x+=x;else break}return s;}
+  if(!(this instanceof xmlNode)){
+    return new xmlNode(xmlString, identifiers, depth, parentID)
+  }
+  
+    
+  function repeat(x,n){
+    //repeats a string, x, n times
+    var s='';for (;;) {if(n&1)s+=x;n>>=1;if(n) x+=x;else break}return s;
+  }
 
   /*
    * an object representing a node in an xml object. Has children indexed by their IDs,
@@ -9,7 +16,7 @@ xmlNode = function (xmlString, identifiers, depth, parentID) {
    
   var node = this;
 
-  this.outerXML    = "" + (xmlString.indexOf("\n") == 0 ? "" : "\n") + repeat(" ",4*depth) + xmlString;
+  this.outerXML    = "";
   this.identifiers = {};
   this.children    = {};
   this.childKeys   = [];
@@ -21,9 +28,14 @@ xmlNode = function (xmlString, identifiers, depth, parentID) {
   this.UID         = "";
   this.indent      = "";
   
-  this.length      = function () { return Object.keys(this.children).length };
+  this.length = function() {
+    //the length of a node will be defined by the number of children
+    return Object.keys(this.children).length
+  };
 
   var __construct = function (self) {
+    if (xmlString) self.outerXML = (xmlString.indexOf("\n") == 0 ? "" : "\n") + repeat(" ",4*depth) + xmlString;
+    
     if (x = self.outerXML.match(/<\?[\s\S]*\?>/)) self.declaration = x[0];
     
     //x = self.outerXML.match(/<(\w+)\/>/);
@@ -63,9 +75,18 @@ xmlNode = function (xmlString, identifiers, depth, parentID) {
       self.UID = self.tagName + "." + self.innerXML;
     }
   }(this);
+  
+    
+
+  
 
   this.diff = function (newNode) {
-    if(node === this) return new node.diff(newNode);
+    /*
+     * node.diff represents the difference between the current node and another node
+     */
+    console.log(node);
+    
+    if(!(this instanceof node.diff)) return new node.diff(newNode);
     
     var oldNode   = node;
     var self      = this;
@@ -75,6 +96,7 @@ xmlNode = function (xmlString, identifiers, depth, parentID) {
     this.new      = newNode;
     this.status   = 0;
     this.children = {};
+    
     var status = { 
       "ignore": 0,
       "unchanged": 1,
@@ -83,6 +105,8 @@ xmlNode = function (xmlString, identifiers, depth, parentID) {
       "modified": 8,
       "childrenModified": 16
     };
+    
+    
     var __construct = function(self){
       if (oldNode && !newNode) {
         self.status = status.deleted;
@@ -100,8 +124,8 @@ xmlNode = function (xmlString, identifiers, depth, parentID) {
         });
         for (var i in keys){
           var k = keys[i];
-          var oldChild = oldNode.childKeys.indexOf(k)<0 ? false : oldNode.children[k];
-          var newChild = newNode.childKeys.indexOf(k)<0 ? false : newNode.children[k];
+          var oldChild = oldNode.childKeys.indexOf(k)<0 ? xmlNode() : oldNode.children[k];
+          var newChild = newNode.childKeys.indexOf(k)<0 ? xmlNode() : newNode.children[k];
           
           self.children[k] = oldChild.diff(newChild);
         }
@@ -110,23 +134,57 @@ xmlNode = function (xmlString, identifiers, depth, parentID) {
       }
     }(this);
     
+    
     this.filter = function(terms){
+      /*
+       * filter the diff by an array of terms, such that only modifications,
+       * additions, and deletions where the ID matches one of the terms are
+       * considered
+       */
       var matches = false;
       for (var i in terms) { if (self.UID.indexOf(terms[i]) != -1) matches = true };
       if (self.status != status.unchanged 
         && self.status != status.childrenModified
         && !matches
       ) {
-        if (self.status == status.added) self.status = status.ignore;
-        else {self.status = status.unchanged}
-      } else if (self.status == status.childrenModified
-        && !matches
-      ) {
+        if (self.status == status.added) {
+          self.status = status.ignore;
+        } else {
+          self.status = status.unchanged;
+        }
+      } else if (self.status == status.childrenModified && !matches) {
         for (var i in self.children) { self.children[i].filter(terms); }
       }
     }
     
-    this.toString = function(){
+    
+    this.toString = function(sortFunction){
+      /*
+       * Return a string which is a valid diff of the XML files, with + prepended
+       * to added elements, and - prepended to removed elements.
+       * If sortFunction is provided, elements will be sorted using that function.
+       */
+       
+      if(! sortFunction) {
+        sortFunction = function(a,b){
+          /*
+           * this sort function mimics the way that Salesforce.com XML files are
+           * sorted.
+           */
+          if (
+            //sort IP addresses sensibly
+            (aIP = a.match(/\.(\d+)\.(\d+)\.(\d+)\.(\d+)$/)) 
+            && (bIP = b.match(/\.(\d+)\.(\d+)\.(\d+)\.(\d+)$/))
+          ) {
+            return (aIP[1]*0x1000000 + aIP[2]*0x10000 + aIP[3]*0x100 + aIP[4]*1) 
+            - (bIP[1]*0x1000000 + bIP[2]*0x10000 + bIP[3]*0x100 + bIP[4]*1);
+          } else {
+            //sort, ignoring spaces
+            return a.replace(/ /g,"") > b.replace(/ /g,"") ? 1 : -1;
+          }
+        }
+      }
+        
       if (self.status == status.unchanged) {
         return self.old.outerXML.replace(/\n/g,"\n ");
       } else if (self.status == status.added) {
@@ -137,21 +195,7 @@ xmlNode = function (xmlString, identifiers, depth, parentID) {
         return self.old.outerXML.replace(/\n/g,"\n-") 
           + self.new.outerXML.replace(/\n/g,"\n+");
       } else if (self.status == status.childrenModified) {
-        
-        var allKeys = Object.keys(self.children).sort(function(a,b){
-          if (
-            //sort IP addresses sensibly
-            (aIP = a.match(/\.(\d+)\.(\d+)\.(\d+)\.(\d+)$/)) 
-            && (bIP = b.match(/\.(\d+)\.(\d+)\.(\d+)\.(\d+)$/))
-          ) {
-            return (aIP[1]*0x1000000 + aIP[2]*0x10000 + aIP[3]*0x100 + aIP[4]*1) 
-            - (bIP[1]*0x1000000 + bIP[2]*0x10000 + bIP[3]*0x100 + bIP[4]*1);
-          } else {
-            //sort, ignoring spaces
-            return a.replaceAll(" ","") > b.replaceAll(" ","") ? 1 : -1;
-          }
-        });
-        
+        var allKeys = Object.keys(self.children).sort(sortFunction);
         var output = self.old.declaration ? "\n " + self.old.declaration : "" ;
         output += "\n " + repeat("    ",self.old.depth) + "<" + self.tagName;
         if (self.old.properties) output += self.old.properties;
