7a767398fbe850db439a40575f31eb27618ab40c
Alexander Brett <alexander.brett@sophos.com>
2014-08-07 17:08:48 +0100

    #1 Kill references to UID in xmlNode; write uidGen

--- js/xmlNode.js
+++ js/xmlNode.js
@@ -1,45 +1,44 @@
-xmlNode = function (xmlData, identifiers, depth) {
-  if(!(this instanceof xmlNode)){
-    return new xmlNode(xmlData, identifiers, depth)
-  }
-  
-    
-  function repeat(x,n){
-    //repeats a string x n times
-    var s='';for (;;) {if(n&1)s+=x;n>>=1;if(n) x+=x;else break}return s;
-  }
+xmlNode = function (xmlData, depth) {
 
   /*
    * an object representing a node in an xml object. Has children indexed by their IDs,
    * as defined by the identifiers object argument
    */
    
+  // regardless of how it was called, return an instance
+  if (!(this instanceof xmlNode)) return new xmlNode(xmlData, depth);
+  
+  
+  var repeat = function (x,n) { //repeats a string x n times, fast. Used for indentation.
+    var s='';for(;;){if(n&1)s+=x;n>>=1;if(n)x+=x;else break}return s;
+  }
+  
+  
   var node = this;
 
   this.outerXML    = "";
-  this.identifiers = {};
-  this.children    = {};
-  this.childKeys   = [];
+  this.children    = [];
   this.depth       = depth || 0;
   this.declaration = "";
   this.tagName     = "";
   this.innerXML    = "";
   this.properties  = "";
-  this.UID         = "";
   this.indent      = "";
   this.content     = "";
   
-  //the following regex
+  //the following regex identifies xml components
   var xmlGlobalRegex = /(<\?[\s\S]*\?>\s*)?<(\w+)( [^>]*)?(?:\/>|>([\s\S]*)<\/\2>)/g;
   
-  this.length = function() {
-    //the length of a node will be defined by the number of children
-    return Object.keys(this.children).length
-  };
-  
   var __construct = function(){
+    /*
+     * Recursively build the xmlNode, based on parsing the outer xml, then parsing
+     * the inner xml, each time passing an array of matches into the child constructor
+     * in order to reduce the number of times the regex gets called.
+     */
+     
     var xmlArray;
     var matches;
+    
     if (typeof xmlData === "string") {
       node.outerXML = "\n" + xmlData.trim();
       xmlArray = /(<\?[\s\S]*\?>\s*)?<(\w+)( [^>]*)?(?:\/>|>([\s\S]*)<\/\2>)/.exec(node.outerXML);
@@ -53,45 +52,25 @@ xmlNode = function (xmlData, identifiers, depth) {
       node.tagName     = xmlArray[2] || "";
       node.properties  = xmlArray[3] || "";
       node.innerXML    = xmlArray[4] || "";
-      if(identifiers && Object.keys(identifiers).indexOf(node.tagName) > -1){
-        node.identifiers = identifiers[node.tagName];
-      }
     }
     
     while (matches = xmlGlobalRegex.exec(node.innerXML)) {
-        var child = xmlNode(matches, node.identifiers, node.depth + 1);
-        node.children[child.UID] = child;
+        node.children.push(xmlNode(matches, node.depth+1););
     }
     
-    node.childKeys = Object.keys(node.children).sort();
-    
-    if(Object.keys(node.identifiers).indexOf("ID") > -1){
-      node.UID = node.tagName;
-      for (var i in node.identifiers.ID) {
-        for (var j in node.childKeys){
-          if (node.childKeys[j].indexOf(node.identifiers.ID[i]) > -1){
-            node.UID += "." + node.childKeys[j];
-          }
-        }
-      }
-    } else /*0if (self.length() == 0)*/{
-      node.UID = node.tagName + "." + node.innerXML;
-    }
-    
-    node.content = function(){
-      if((l = node.length())>0){
-        var output = "\n" + node.declaration + "<" + node.tagName + node.properties + ">";
-        for (i=0; i<l; i++) {
-          output += node.children[node.childKeys[i]].content;
-        }
-        output += "\n</" + node.tagName + ">";
-        return output;
-      } else {
-        return node.outerXML;
-      }
-    }();
-  
   }();
 
-  
+  node.formatted = function(){
+    if ((l = node.children.length) > 0) {
+      
+      var output = "\n" + node.declaration + "<" + node.tagName + node.properties + ">";
+      for (i=0; i<l; i++) output += node.children[i].formatted();
+      output += "\n</"+node.tagName+">";
+      return output;
+      
+    } else {
+      return node.outerXML;
+    }
+  };
+
 };
--- js/xmlNode.uidDiff.js
+++ js/xmlNode.uidDiff.js
@@ -1,15 +1,57 @@
-xmlNode.prototype.uidDiff = function (newNode,oldNode) {
+xmlNode.prototype.uidGen = function (schema) {
+  /*
+   * uidGen adds the UID, childUIDs, and childrenByUID attributes to the node.
+   *
+   * The UID is generated according to the schema, the children's UIDs are
+   * enumerated in childUIDs, and childrenByUID maps UIDs to children.
+   *
+   * This function could be overridden provided those constraints held.
+   */
+
+  if (!this.UID) {
+    
+    this.UID = this.tagName;
+    this.childUIDs = [];
+    this.childrenByUID = {};
+    
+    if (schema && Object.keys(schema).indexOf(this.tagName) > -1) {
+      schema = schema[node.tagName];
+    }
+    
+    for (i=0; i<this.children.length; i++) {
+      var UID = this.children[i].uidGen(schema);
+      this.childrenByUID[UID] = this.children[i];
+      this.childUIDs.push(UID);
+    } 
+    
+    if(Object.keys(schema).indexOf("ID") > -1){
+      for (var i in schema.ID) {
+        for (var j in this.childUIDs) {
+          if (this.childUIDs[j].indexOf(schema.ID[i]) > -1) {
+            this.UID += "." + this.childUIDs[j];
+          }
+        }
+      }
+    } else {
+      this.UID += "." + this.innerXML;
+    }
+  }
+  
+  return this.UID;
+}
+
+xmlNode.prototype.uidDiff = function (identifiers, newNode, oldNode) {
   /*
    * xmlNode.uidDiff represents the difference between the current node and another node.
    * The algorithm requires some way of assigning a unique identifier to each node.
    */
   
-  if(this instanceof xmlNode) return new this.uidDiff(newNode,(oldNode || this));
+  if(this instanceof xmlNode) return new this.uidDiff(identifiers, newNode, (oldNode || this));
   
   var diff      = this;
   diff.old      = oldNode;
   diff.new      = newNode;
-  diff.UID      = (diff.old || diff.new).UID;
+  diff.UID      = "";
   diff.tagName  = (diff.old || diff.new).tagName;
   diff.status   = 0;
   diff.children = {};
@@ -29,11 +71,13 @@ xmlNode.prototype.uidDiff = function (newNode,oldNode) {
       diff.status = status.deleted;
     } else if (diff.new && !diff.old) {
       diff.status = status.added;
-    } else if (diff.old.content == diff.new.content) {
+    } else if (diff.old.formatted() == diff.new.formatted()) {
       diff.status = status.unchanged;
-    } else if (diff.old.properties == diff.new.properties
+    } else if (
+      diff.old.properties == diff.new.properties
       && diff.old.declaration == diff.new.declaration
-      && diff.old.length() > 0
+      && diff.old.children.length > 0
+      && diff.new.children.length > 0
     ) {
       diff.status = status.childrenModified;
       var keys = diff.old.childKeys.concat(diff.new.childKeys).filter(function(e, i, array) {
@@ -103,14 +147,14 @@ xmlNode.prototype.uidDiff = function (newNode,oldNode) {
     }
       
     if (diff.status == status.unchanged) {
-      return diff.old.content.replace(/\n/g,"\n ");
+      return diff.old.formatted().replace(/\n/g,"\n ");
     } else if (self.status == status.added) {
-      return diff.new.content.replace(/\n/g,"\n+");
+      return diff.new.formatted().replace(/\n/g,"\n+");
     } else if (diff.status == status.deleted) {
-      return diff.old.content.replace(/\n/g,"\n-");
+      return diff.old.formatted().replace(/\n/g,"\n-");
     } else if (diff.status == status.modified) {
-      return diff.old.content.replace(/\n/g,"\n-") 
-        + diff.new.content.replace(/\n/g,"\n+");
+      return diff.old.formatted().replace(/\n/g,"\n-") 
+        + diff.new.formatted().replace(/\n/g,"\n+");
     } else if (diff.status == status.childrenModified) {
       var allKeys = Object.keys(diff.children).sort(sortFunction);
       var output = diff.old.declaration ? "\n " + diff.old.declaration : "" ;
